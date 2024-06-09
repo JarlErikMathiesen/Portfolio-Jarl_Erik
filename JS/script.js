90e96d905152a3bad953ffefebdf54961f173995
JarlErikMathiesen <117684143+JarlErikMathiesen@users.noreply.github.com>
2024-06-09 10:30:52 +0200

    Fixed about-box

--- JS/script.js
+++
@@ -1,7 +0,0 @@
-const portfolioCards = document.querySelectorAll(".portfolio-card");
-
-portfolioCards.forEach(function(card) {
-    card.addEventListener("click", function() {
-        card.style.zIndex = "50";
-    });
-});
